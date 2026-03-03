54d4846129606f6957e4fa5545ffd001a70ebcba
mou-kexin <2584857586@qq.com>
2026-03-03 22:37:59 +0800

    自动化测试学习代码

--- src/test/java/com/bite/autotest/AutoTestApplicationTests.java
+++ src/test/java/com/bite/autotest/AutoTestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AutoTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
