75cc0bc9f025a3abd97015225b55146c3ea2d6d9
mou-kexin <2584857586@qq.com>
2026-03-04 22:05:08 +0800

    自动化测试学习代码

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,3 +1,35 @@
-public class firstTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+public class firstTest {
+    public void test_dilireba() throws InterruptedException {
+        // 驱动程序管理的自动化
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        // 允许访问所有链接
+        options.addArguments("--remote-allow-origins=*");
+
+        // 1.打开浏览器
+        WebDriver driver = new ChromeDriver(options);
+        Thread.sleep(3000);
+
+        // 2.输入网址
+        driver.get("https://www.baidu.com");
+        Thread.sleep(3000);
+
+        // 3.找到输入框并输入“迪丽热巴”
+        driver.findElement(By.xpath("//*[@id=\"kw\"]")).sendKeys("迪丽热巴");
+        Thread.sleep(3000);
+
+        // 4.找到“百度一下”按钮并点击
+        driver.findElement(By.xpath("//*[@id=\"su\"]")).click();
+        Thread.sleep(3000);
+
+        // 5.关闭浏览器
+        driver.quit();
+    }
 }
