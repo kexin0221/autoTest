0b5978dce6fe23d5e1f9e5b6b2d15a068d238a54
mou-kexin <2584857586@qq.com>
2026-03-06 22:05:15 +0800

    自动化测试学习代码

--- src/test/java/secondTest.java
+++ src/test/java/secondTest.java
@@ -1,8 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
